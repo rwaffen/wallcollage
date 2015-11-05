0a68d4d0f9ee70b2b73e67c994cd3a85e5ea8b3c
Robert Waffen <rwaffen@gmail.com>
2015-11-05 16:22:03 +0100

    refactor all the code!
    - use rmagick instead of minimagick
    - add menu
    - make the code somewhat flexible

--- README.md
+++ README.md
@@ -4,11 +4,17 @@ Pick randomly pictures from a directory path and join them to a 2560x1440 collag
 
 # Installation
 
-    mkdir out
     bundle install --path vendor/gems
 
 # Usage
 
-    bundle exec ruby wallcollage.rb
+    Usage: wallcollage2.rb [-qp]
+        -q, --quantity                   quantity to include in the collage.
+        -p, --path                       The path to the picture folder.
 
-pick the images from the out/ directory
+    bundle exec ruby wallcollage2.rb -q 10 -p /Users/Rob/Dropbox/Bilder/dump/2015
+
+At the moment there are only standard values for a quantity of 10 and 40.
+The images will be resized to fill a sub-resolution of the whole collage.
+Best are images with width < height
+This will conclude in a collage of 2560x1440. Pick the images from the out/ directory
--- wallcollage2.rb
+++ wallcollage2.rb
@@ -1,19 +1,62 @@
-require 'mini_magick'
+require 'rmagick'
 require 'pp'
+require 'choice'
 
-def get_rnd_files
-  Dir["/Users/Rob/Dropbox/Bilder/dump/2015/*.jpg"]
+Choice.options do
+  separator ''
+
+  option :quantity, :required => true do
+    short '-q'
+    long  '--quantity'
+    desc  'quantity to include in the collage.'
+  end
+
+  option :path , :required => true do
+    short '-p'
+    long  '--path'
+    desc  'The path to the picture folder.'
+  end
 end
 
-directory = get_rnd_files.sample(10)
+output_dir= 'out/'
+Dir.mkdir(output_dir) unless File.exists?(output_dir)
 
-montage = MiniMagick::Tool::Montage.new
-directory.each { |image| montage << image }
-montage << '-background'
-montage << 'none'
-montage << '-geometry'
-montage << '512x720'
-montage << '-tile'
-montage << '5x'
-montage << Dir.pwd + "/out/#{SecureRandom.hex}.jpg"
-montage.call
+def get_rnd_files(path, quantity)
+  list = Dir[path + '/*.jpg']
+  list += Dir[path + '/*.png']
+  list.sample(quantity)
+end
+
+def resize_images(image_list, width, height)
+  tmp_list = Magick::ImageList.new
+  gravity = Magick::NorthGravity
+  image_list.each do |image|
+    tmp_image = image.resize_to_fill(width, height, gravity)
+    tmp_list << tmp_image
+  end
+  tmp_list
+end
+
+def create_collage(path,quantity)
+  if quantity == 40
+    width = 256
+    height = 360
+    tile = '10x'
+  elsif quantity == 10
+    width = 512
+    height = 720
+    tile = '5x'
+  end
+
+  list = Magick::ImageList.new(*get_rnd_files(path,quantity))
+  output_list = resize_images(list, width, height)
+
+  collage = output_list.montage do |mont|
+    mont.geometry = "#{width}x#{height}"
+    mont.tile = tile
+  end
+
+  collage.write(Dir.pwd + "/out/#{SecureRandom.hex}.jpg")
+end
+
+create_collage Choice[:path], Choice[:quantity].to_i
