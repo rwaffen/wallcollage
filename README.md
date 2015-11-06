7dd40ed112fbc327ccb76225dd287c3d0a260ed9
Robert Waffen <rwaffen@gmail.com>
2015-11-06 04:14:43 +0100

    refactor

--- README.md
+++ README.md
@@ -6,21 +6,98 @@ Pick randomly pictures from a directory path and join them to a 2560x1440 collag
 
 ## Mac OS X
 
+### Prerequisites
+
     brew install imagemagick
     brew install ghostscript
     gem install bundler
+
+### Main installation
+
     bundle install --path vendor/gems
 
 # Usage
+
+## Command parameters
 
     Usage: wallcollage2.rb [-qp]
         -q, --quantity                   quantity to include in the collage.
         -p, --path                       The path to the picture folder.
 
+## Example usage
 
-    bundle exec ruby wallcollage2.rb -q 10 -p /Users/Rob/Dropbox/Bilder/dump/2015
+    bundle exec ruby wallcollage2.rb -p /Users/Rob/Pictures/dump/ -q 19
+    
+    bundle exec ruby wallcollage2.rb -p /Users/Rob/Pictures/dump/ -q 55
+    
+    bundle exec ruby wallcollage2.rb -p /Users/Rob/Pictures/dump/ -q 5
+    
+    bundle exec ruby wallcollage2.rb -p /Users/Rob/Pictures/dump/ -q 22
 
-At the moment there are only standard values for a quantity of 10 and 40.
-The images will be resized to fill a sub-resolution of the whole collage.
-Best are images with width < height
-This will conclude in a collage of 2560x1440. Pick the images from the out/ directory
+The images will be resize to fill a sub-resolution of the whole collage.
+This will conclude in a collage of 2560x1440. 
+Pick the images from the out/ directory
+
+# Supported Units
+
+## Image formats
+
+Support for jpg and png only. Preferred picture format should be portrait style. (Best are images with width < height)
+
+## Quantity of images from 1 to 55
+
+    not supported quantity
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+    not supported quantity
+    12
+    not supported quantity
+    14
+    15
+    16
+    not supported quantity
+    18
+    not supported quantity
+    20
+    21
+    22
+    not supported quantity
+    24
+    25
+    not supported quantity
+    27
+    28
+    not supported quantity
+    30
+    not supported quantity
+    32
+    33
+    not supported quantity
+    35
+    36
+    not supported quantity
+    not supported quantity
+    39
+    40
+    not supported quantity
+    42
+    not supported quantity
+    44
+    45
+    not supported quantity
+    not supported quantity
+    48
+    49
+    50
+    51
+    52
+    not supported quantity
+    54
+    55
--- wallcollage2.rb
+++ wallcollage2.rb
@@ -3,18 +3,36 @@ require 'pp'
 require 'choice'
 
 Choice.options do
-  separator ''
+  header ''
+  header 'Specific options:'
 
   option :quantity, :required => true do
     short '-q'
-    long  '--quantity'
-    desc  'quantity to include in the collage.'
+    long '--quantity'
+    desc 'quantity to include in the collage.'
   end
 
-  option :path , :required => true do
+  option :path, :required => true do
     short '-p'
-    long  '--path'
-    desc  'The path to the picture folder.'
+    long '--path'
+    desc 'The path to the picture folder.'
+  end
+
+  separator ''
+  separator 'Common options: '
+
+  option :width, :required => false do
+    short '-w'
+    long '--width'
+    desc 'The width of the collage. Defaults to 2560.'
+    default 2560
+  end
+
+  option :height, :required => false do
+    short '-h'
+    long '--height'
+    desc 'The height of the collage. Defaults to 1440.'
+    default 1440
   end
 end
 
@@ -27,9 +45,8 @@ def get_rnd_files(path, quantity)
   list.sample(quantity)
 end
 
-def resize_images(image_list, width, height)
+def resize_images(image_list, width, height, gravity = Magick::NorthGravity)
   tmp_list = Magick::ImageList.new
-  gravity = Magick::NorthGravity
   image_list.each do |image|
     tmp_image = image.resize_to_fill(width, height, gravity)
     tmp_list << tmp_image
@@ -37,26 +54,53 @@ def resize_images(image_list, width, height)
   tmp_list
 end
 
-def create_collage(path,quantity)
-  if quantity == 40
-    width = 256
-    height = 360
-    tile = '10x'
-  elsif quantity == 10
-    width = 512
-    height = 720
-    tile = '5x'
-  end
-
-  list = Magick::ImageList.new(*get_rnd_files(path,quantity))
-  output_list = resize_images(list, width, height)
-
-  collage = output_list.montage do |mont|
-    mont.geometry = "#{width}x#{height}"
-    mont.tile = tile
-  end
-
-  collage.write(Dir.pwd + "/out/#{SecureRandom.hex}.jpg")
+def get_opts(quantity, multiplier, max_width, max_height)
+  #max_width = max_width - ((quantity / multiplier) * 2)
+  #max_height = max_height - (multiplier * 2)
+  {
+      :width => max_width / (quantity / multiplier),
+      :height => max_height / multiplier,
+      :tile => "#{quantity / multiplier}x#{multiplier}",
+  }
 end
 
-create_collage Choice[:path], Choice[:quantity].to_i
+def create_collage(path, quantity, max_width, max_height)
+
+  case quantity
+    when 2..7, 9
+      opts = get_opts quantity, 1, max_width, max_height
+    when 8, 10, 12, 14, 16, 20, 22
+      opts = get_opts quantity, 2, max_width, max_height
+    when 15, 18, 21, 24, 27, 30, 33, 39, 42, 51
+      opts = get_opts quantity, 3, max_width, max_height
+    when 24, 28, 32, 36, 40, 44, 48, 52
+      opts = get_opts quantity, 4, max_width, max_height
+    when 25, 35, 45, 50, 55
+      opts = get_opts quantity, 5, max_width, max_height
+    when 54
+      opts = get_opts quantity, 6, max_width, max_height
+    when 49
+      opts = get_opts quantity, 7, max_width, max_height
+    else
+      puts 'not supported quantity'
+      exit 1
+  end
+
+  list = Magick::ImageList.new(*get_rnd_files(path, quantity))
+  output_list = resize_images(list, opts[:width], opts[:height])
+
+  collage = output_list.montage do |mont|
+    #mont.geometry = "#{opts[:width]}x#{opts[:height]}"
+    mont.geometry = "#{opts[:width]}x#{opts[:height]}+1+1"
+    mont.tile = opts[:tile]
+  end
+
+  collage.write(Dir.pwd + "/out/#{quantity}_#{SecureRandom.hex}.jpg")
+end
+
+create_collage(
+    Choice[:path],
+    Choice[:quantity].to_i,
+    Choice[:width].to_i,
+    Choice[:height].to_i
+)
